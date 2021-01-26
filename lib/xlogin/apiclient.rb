235afebbea6bc1d201e51b5d2b6ed225b5cce90e
haccht <haccht@users.noreply.github.com>
2021-01-26 17:30:38 +0900

    Bump major version

--- lib/xlogin/apiclient.rb
+++ lib/xlogin/apiclient.rb
@@ -1,78 +1,44 @@
 require 'json'
 require 'net/http'
-require 'ostruct'
+require 'addressable/uri'
 require "xlogin/apiclient/version"
 
 module Xlogin
-  module APIClient
-    class APIError < StandardError; end
+  class APIClient
+    class Error < StandardError; end
 
-    class Client
-      def initialize(base_uri, type:, **args)
-        @uri  = base_uri
-        @type = type
-        @args = args
-      end
-
-      def cmd(req = Request.new, &block)
-        if req.kind_of?(String)
-          req, _req = Request.new, req
-          req.command = _req
-        end
-
-        req.xlogin = @args
-        block.call(req) if block
-
-        uri = @uri.dup
-        uri.path += "/vendors/#{@type}/actions"
-
-        http(:post, uri, req.to_h)
-      rescue => e
-        raise APIError.new(e.message)
-      end
-
-      private
-      def http(method, uri, **args)
-	http = Net::HTTP.new(uri.host, uri.port)
-
-	klass = Net::HTTP.const_get(method.to_s.capitalize)
-	req  = klass.new(uri.path)
-	req["Accept"] = "application/json"
-
-	unless args.empty?
-	  req["Content-Type"] = "application/json"
-	  req.body = JSON.dump(args)
-	end
-
-	resp = http.request(req)
-	body = resp.body
-
-        Response.new(body.nil? ? {} : JSON.parse(body))
-      end
+    class << self
+      attr_accessor :base_url
     end
 
-    class Factory
-      def initialize(uri)
-	@base_uri = URI(uri)
-      end
+    def initialize(base_url: self.class.base_url, **args)
+      raise Error.new('base_url not defined') unless base_url
 
-      def create(**args)
-        Client.new(@base_uri, args)
-      end
+      @uri  = Addressable::URI.parse(base_url)
+      @type = args.delete(:type)
+      @args = args
     end
 
-    class Request < OpenStruct
-      def initialize(**args)
-        super(*args)
-        self.captures ||= []
-      end
-
-      def to_h
-        super.delete_if { |k, v| k == :captures && v.empty? }
-      end
+    def cmd(args)
+      params = {driver: @type, target: @args, command: args}
+      resp = request(**params.transform_keys(&:to_sym))
+      resp['payload'].join
     end
 
-    class Response < OpenStruct
+    private
+    def request(**params)
+      uri = @uri.dup
+      uri.query = "q=#{URI.encode_www_form_component(JSON.generate(params))}"
+
+      req = Net::HTTP::Get.new(uri.request_uri)
+      req["Accept"] = "application/json"
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      body = http.request(req).body
+      resp = body ? JSON.parse(body) : {}
+      return resp if resp['success']
+
+      raise Error.new(resp['error'])
     end
   end
 end
--- lib/xlogin/apiclient/version.rb
+++ lib/xlogin/apiclient/version.rb
@@ -1,5 +1,5 @@
 module Xlogin
   module Apiclient
-    VERSION = "0.1.6"
+    VERSION = "0.2.0"
   end
 end
